0f06e9a765b8bdf4aed8c10cc71d935bb878bc18
GT013 <42264121+GT013@users.noreply.github.com>
2020-10-22 23:38:31 +0700

    modify

--- myapp.py
+++
@@ -1,6 +0,0 @@
-from wit import Wit
-
-wit_access_token = "6VK62DOSFZVG5G3NUFOPI5LRPAIQTLVT"
-client = Wit(access_token=wit_access_token)
-resp = client.message("เอกสารกู้ยืม กยศ ใช้อะไรบ้าง")
-print(resp)
