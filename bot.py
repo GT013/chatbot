73f695bf1a231a592fb25634d787b327c8272096
GT013 <42264121+GT013@users.noreply.github.com>
2020-11-12 22:05:45 +0700

    add

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from wit import Wit
 
-wit_access_token = "6VK62DOSFZVG5G3NUFOPI5LRPAIQTLVT"
+wit_access_token = "PFZOON223DGRVC2DZXCIJG3CPJH4ASBG"
 client = Wit(access_token=wit_access_token)
 
 def wit_response(message_text):
@@ -9,10 +9,14 @@ def wit_response(message_text):
 	entity = None
 	
 	try:
+		#entity = list(resp['entities'])[0]
 		entity = list(resp['entities'])[0]
 	except:
 		pass
 
 	return (entity)
 
-#print(wit_response("สวัสดีค่ะ อยากทราบว่าสถาบันมีทุนอะไรบ้างคะ"))
+#print(wit_response("กำหนดการการกู้กยศ."))
+
+#resp = client.message("สวัสดีนะ")
+#print(resp)
